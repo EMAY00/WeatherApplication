4d7bcc8764a6fa9780c1f49b838379a520501d86
ElennMayAdeliine Pahk <ElennMayAdeliine.Pahk@tptlive.ee>
2019-08-29 07:35:42 +0200

    yep

---
+++ WeatherApp/WeatherApp/obj/Debug/81/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: aa34a83f06373a7da5ef35faddb33fddb79b483a */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	924
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 242089
+	.include	"typemap.jm.inc"
---
+++ WeatherApp/WeatherApp/obj/Debug/81/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: aa34a83f06373a7da5ef35faddb33fddb79b483a */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	924
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 242089
+	.include	"typemap.jm.inc"
